2c471ded34faf14ef92e5ea3a47ef69e055e0195
Bartowski07 <126920086+Bartowski07@users.noreply.github.com>
2023-04-03 11:43:50 +0200

    added a few jokes

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-const name = 'Chuck';
-console.log(`Hello, ${name}!`);
+const knocks = ["Knock, knock. Who's there?\n"];
+
+let answers = [
+  "-Ice cream.\n Ice cream who?\n -Ice cream if you don't give me some candy!",
+  "-Stopwatch.\n Stopwatch who?\n -Stopwatch you're doing and let me in!",
+  "-Spell.\n Spell who?\n -W. H. O.",
+  "-Icy.\n Icy who?\n -Icy you looking at me!",
+  "-Norma Lee.\n Norma Lee who?\n -Norma Lee I don't knock on random doors, but I had to meet you!",
+  "-Somebody too short to ring the doorbell!",
+  "-Harry.\n Harry who?\n -Harry up, it’s cold outside!",
+  "-Justin.\n Justin who?\n -Justin time for dinner.",
+  "-Isabel.\n Isabel who?\n -Isabel working?",
+  "-I am.\n I am who?\n -Wait, you don’t know who you are?",
+];
+
+function generateJoke() {
+  const answer = answers[Math.floor(Math.random() * answers.length)];
+
+  return `${knocks} ${answer}`;
+}
+
+console.log(generateJoke());
